2b649a1c2ead6ec152f3bb79ef4fd626b07a32d0
HediaTnani <59562743+HediaTnani@users.noreply.github.com>
2021-01-28 14:44:28 +0100

    Add files via upload

--- 02_code/install_packages.R
+++ 02_code/install_packages.R
@@ -1,1 +1,9 @@
+install.packages("ggplot2")
+install.packages("gridExtra")
+install.packages("BiocManager")
+install.packages("reshape2")
+BiocManager::install(c("mixOmics", "DESeq2", "edgeR", "HTSFilter", "GOplot", "DEFormats", "vsn"))
+install.packages("RColorBrewer")
+install.packages(c("VennDiagram","devtools", "tidyverse","readr","Hmisc","corrplot","gridExtra","pheatmap"))
+
 
